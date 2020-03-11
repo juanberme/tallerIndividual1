7fbcb1dc9a2f91e7b5b589a67fe2c51d77b84a21
juan camilo bermeo <camilo22601@gmail.com>
2020-03-11 07:04:59 -0500

    commit3

---
+++ tesla/src/tesla/pantallas.java
@@ -0,0 +1,26 @@
+package tesla;
+
+public class pantallas {
+	
+	protected int posX;
+	protected int posY;
+	protected int tamX;
+	protected int tamY;
+	
+	public pantallas(int posX, int posY, int tamX, int tamY) {
+		
+		this.posX=posX;
+		this.posY=posY;
+		this.tamX=tamX;
+		this.tamY=tamY;
+	}
+
+	public void crearPantalla() {
+		
+	}
+	
+	public void cambioPantalla() {
+		
+	}
+
+}
